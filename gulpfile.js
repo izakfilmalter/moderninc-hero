ad50ed97f58544efe6c62d26c41cece89e990cde
foxed2 <foxed2mail@gmail.com>
2015-01-06 00:56:15 +0100

    Added comments to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp         = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
 	react        = require('gulp-react');
 
+// Sass task : $ gulp sass	 to run
 gulp.task('sass', function() {
 	gulp.src('sass/style.sass')
 	.pipe(sass({
@@ -10,31 +11,31 @@ gulp.task('sass', function() {
 		debugInfo: false //use Firesass Firebug plugin if true
 	}))
 	.on('error', function (err) { console.log(err.message); })
-	// .pipe(autoprefixer({
-	// 	browsers: ['last 2 versions'],
-	// 	cascade: true //for nice and pretty vendor prefixe cascades
-	// }))
 	.pipe(gulp.dest('public/css'));
 });
 
+// Autoprefixer task : $ gulp prefix	to run
 gulp.task('prefix', function() {
 	gulp.src('public/css/main.css')
 		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
+			browsers: ['last 2 versions'], //see documentation to expand compability
+			cascade: true //for nice and beautiful cascades
 		}))
 		.pipe(gulp.dest('public/css/main.css'));
 });
 
+// React procompiler task : $ gulp react 	to run
 gulp.task('react', function() {
 	gulp.src('pathtoreplace/filetoreplace.jsx')
 		.pipe(react())
 		.pipe(gulp.dest('destinationpath'));
 });
 
+// File watchers
 gulp.watch('sass/**/*.sass', ['sass']);
 gulp.watch('public/css/*.css', ['prefix']);
 
+// Default task : $ gulp 	to run
 gulp.task('default', [
 					'sass'
 					//no need to call 'prefix'. It will trigger after sass task has finished
