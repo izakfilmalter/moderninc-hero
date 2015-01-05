71d71c4e89b294c83c4424b43704b127780e16f2
izakfilmalter <izak@whoatech.com>
2015-01-05 15:20:45 -0800

    Removed grunt. Started adding gulp. Worked on adding in more of modern style. Added in some react stuff.

---
+++ public/head.php
@@ -0,0 +1,14 @@
+<link rel="dns-prefetch" href="http://fb.me/">
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="css/style.css">
+
+<title>Modern Inc.</title>
+
+
+<script src="http://fb.me/react-0.12.2.js"></script>
+<script src="http://fb.me/JSXTransformer-0.12.2.js"></script>
